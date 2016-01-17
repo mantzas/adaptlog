1140a99ab4328daac24746add459de4e328d087b
Sotirios Mantziaris <s.mantziaris@live.com>
2016-01-17 11:04:09 +0200

    added  minimum logger unit test

--- adaptlog_test.go
+++ adaptlog_test.go
@@ -45,11 +45,40 @@ func TestNewMinimumLoggerSucceeds(t *testing.T) {
 
 	minLogger, err := NewMinimumLogger()
 
-	if minLogger != nil {
-		t.Fatal("Logger should have been nil!")
+	if minLogger == nil {
+		t.Fatal("Logger should have been not nil!")
 	}
 
-	if err == nil {
-		t.Fatal("Should have returned a error")
+	if err != nil {
+		t.Fatal("Should not have returned a error")
+	}
+}
+
+func TestNewMinimumLoggerLoggingSucceeds(t *testing.T) {
+
+	var logger = new(TestMinimumLogger)
+
+	ConfigMinimalLogger(logger)
+
+	minLogger, err := NewMinimumLogger()
+
+	if minLogger == nil {
+		t.Fatal("Logger should have been not nil!")
+	}
+
+	if err != nil {
+		t.Fatal("Should not have returned a error")
+	}
+
+	minLogger.Print()
+	minLogger.Printf("Test")
+	minLogger.Println()
+
+	if len(logger.loggingData) != 3 {
+		t.Fatal("Logged items should be 3!")
+	}
+
+	if logger.loggingData[0] != Print || logger.loggingData[1] != Printf || logger.loggingData[2] != Println {
+		t.Fatal("Logged items do not match!")
 	}
 }
